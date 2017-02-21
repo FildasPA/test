4a54fe06a0717017551e18eb9d4edfa4cea09da4
FildasPA <fildas.pa3@gmail.com>
2017-02-21 23:28:28 +0100

    Update

--- segmentation.py
+++ segmentation.py
@@ -2,193 +2,242 @@
 #-*- coding: utf-8 -*-
 
 """
--- Object : Découpe et rassemble des segments d'un fichier audio dans un seul
-            fichier.
--- By : Julien Boge
--- Last modification : 20.02.17
--- Usage : python segmentator.py fichier.mp4 segments.txt
-
-
+-- Objet : Découpe et rassemble des segments d'un fichier audio dans un seul
+           fichier.
+-- Par : Julien Boge
+-- Dernière modification : 21.02.17
 """
 
 import sys
+import pdb
 import os
 import shutil
+import ast
+import argparse
+from argparse import RawTextHelpFormatter
 import subprocess
 
-SUMMARY_FILE      = "_summary"
-# CUT               = "cut"
-DEFAULT_EXTENSION = "mp4"
-SEGMENT_FILES_LIST = "._segment_files_list.txt"
+sys.tracebacklimit    = 1
+
+DESCRIPTION = """
+Extract and join segments of audio and/or video file into one single file.
+
+List of segments times must be specified in milliseconds, either
+into a list or into a file looking as follow:
+0 10340
+25789 36400
+...
+"""
+
+# Flags
+PRINT_STEPS           = True
+PRINT_COMMANDS        = False
+PRINT_COMMANDS_OUTPUT = False
+
+DEFAULT_FORMAT        = "mp4"
+SEGMENTS_FILES_LIST   = "._segment_files_list.txt"
+FINAL_FILE_SUFFIX     = "-summary"
+FINAL_FILE_DEST       = ""
+TEMP_DIR_PREFIX       = "._tmp_segmentation_files_"
+FFMPEG_SILENT         = "-loglevel panic"
+FORCE_OVERWRITING     = ""
 
 
-def file_exists(filepath):
-    """ File exists """
-    return os.path.isfile(filepath)
+def printerror(message):
+    print "Error: {}".format(message)
+
+def printwarning(message):
+    print "Warning: {}".format(message)
 
 def check_file_exists(filepath):
-    """ Check file exists """
-    if not file_exists(filepath):
-        print "'" + filepath + "' doesn't exists!"
+    """ Si le fichier n'existe pas, affiche un message d'erreur
+    et quitte le programme """
+    if not os.path.isfile(filepath):
+        printerror("file '{}' doesn't exist".format(filepath))
         sys.exit(1)
 
-def get_file_name(filepath):
-    """ Get file name """
-    return filepath.split('/')[-1].split('.')[:-1][0]
+def get_segments_times(file):
+    """ Retourne une liste à partir du fichier contenant les
+    intervalles de temps des segments à extraire. """
+    if PRINT_STEPS:
+        print "Get segments from '{}'...".format(file.name)
 
-def get_file_folder_name(filepath):
-    """ Get file folder name """
-    return '/'.join(filepath.split('/')[:-1])
+    times = []
+    for line in file:
+        line = line.split(' ')
+        line = [float(n)/1000 for n in line]
+        times.append([line[0],line[1]])
+    return times
 
-def get_file_ext(filepath):
-    """ Get file extension """
-    return filepath.split('.')[-1]
+def extract_segment(filepath,output_file,format,start_time,end_time):
+    """ Sauvegarde dans un fichier à part un segment du média
+    correspondant à l'intervalle de temps specifié. """
+    if PRINT_STEPS:
+        print "  Extracting from {} to {} (s)...".format(start_time, end_time)
 
-def get_segments_times(filepath):
-    """ Retourne un array à partir du fichier contenant les segments audio à
-    extraire, sous la forme "debut fin", temps en millisecondes.
-    """
-    print "Get segments from " + filepath + "..."
-    file = open(filepath,'r')
-    content = file.readlines()
-    segments_times = [line.strip().split(' ') for line in content]
-    segments_times = [[int(i) for i in segment] for segment in segments_times]
-    return segments_times
+    command = ("ffmpeg {silent} -i {input} -ss {start} -c copy -to {end}"
+               " -f {format} {output}").\
+        format(silent=FFMPEG_SILENT, input=filepath,
+               start=start_time, end=end_time,
+               format=format, output=output_file)
 
-def get_segment(filepath,output_file,ext,start_time,end_time):
-    """ Retourne un segment du média correspondant à l'intervalle de
-    temps specifié.
-    """
-    # duration = end_time - start_time
-
-    print "Extract from {} to {} (ms)".format(start_time, end_time)
-
-    command = 'ffmpeg -loglevel panic -i {input} -ss {start} -c copy -to {end} -f {ext} {output}'.\
-        format(input=filepath, start=start_time,
-               end=end_time, ext=ext, output=output_file)
-
-    print command
-    # print "into: " + output_file
+    if(PRINT_COMMANDS):
+        print command
 
     try:
-        subprocess.check_call(command.split())  # Must split all the arguments.
+        subprocess.check_call(command.split())
     except subprocess.CalledProcessError as exp:
         print('Exception caught: {}'.format(exp))
 
-def get_segments_from_audio(segments_time,audio_origin):
-    """ Recupère, assemble et retourne tous les segments audio specifiés """
-    print "Catching audio segments..."
-    segments = []
-    for time in segments_time:
-        segments.append(get_segment_from_audio(audio_origin,int(time[0]),int(time[1])))
+def extract_segments(filepath,folder,ext,segments_times):
+    """ Créer un dossier temporaire et y stocke les fichiers correspondant
+    aux segments à extraire. """
+    if PRINT_STEPS:
+        print "Extracting segments from '{}'...".format(filepath)
 
-    audio_sum = segments[0]
-    for segment in segments[1:]:
-        audio_sum = audio_sum + segment
-    return audio_sum
+    # Supprime le dossier temporaire existant
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+    # Crée un nouveau dossier temporaire
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
+    number = 0
+    for times in segments_times:
+        output_file = folder + "/cut" + str(number) + "." + ext
+        extract_segment(filepath,output_file,ext,times[0],times[1])
+        number += 1
+    return number
 
-def get_segments(filepath,folder,ext,segments_times):
-    """ Récupère les segments du média dans des fichiers temporaires
-    sauvegardés dans le dossier folder, avant de les assembler dans
-    un seul fichier
-    """
-    segments = []
-    for segment_times in segments_times:
-        output_file = folder + "/cut" + str(len(segments)) + "." + ext
-        segment = get_segment(filepath,output_file,ext,segment_times[0],segment_times[1])
-        segments.append(segment)
-    return segments
+def concat_segments(folder,number,ext,output_file):
+    """ Fusionne les fichiers contenant les segments dans un seul fichier """
+    if PRINT_STEPS:
+        print "Saving media summary into '{}'...".format(output_file)
 
-def concat_segments(folder,output_file):
-    """ Fusionne les fichiers contenant les segments """
-    segments_list = ["file '" + folder + "/" + file + "'"\
-                     for file in list(reversed(os.listdir(folder)))]
+    segments = ["file '" + folder + "/cut" + str(i) + "." + ext\
+                for i in range(0,number)]
 
-    # print "files:\n" + segments_list
-
-    file = open("tmp_segments_files","w")
-    for segment in segments_list:
-        file.write(segment + "\n")
+    # print "files: " + '\n'.join(segments)
+    file = open(SEGMENTS_FILES_LIST,"w")
+    file.write('\n'.join(segments))
+    # for segment in segments:
+        # file.write(segment + "\n")
     file.close()
 
-    command = 'ffmpeg -loglevel panic -f concat -i {list} -c copy {output}'.\
-        format(list="tmp_segments_files",output=output_file)
-    # print "Command:\n" + command
+    command = 'ffmpeg -loglevel panic {force} -f concat -i {list} -c copy {output}'.\
+        format(force=FORCE_OVERWRITING,list=SEGMENTS_FILES_LIST,output=output_file)
+
+    if(PRINT_COMMANDS):
+        print command
+
     try:
-        subprocess.check_call(command.split())  # Must split all the arguments.
+        subprocess.check_call(command.split())
     except subprocess.CalledProcessError as exp:
         print('Exception caught: {}'.format(exp))
 
+    if PRINT_STEPS:
+        print "Saved!"
+
     # Supprime le fichier contenant la liste des chemins vers les segments
-    if os.path.isfile("tmp_segments_files"):
-        os.remove("tmp_segments_files")
+    if os.path.isfile(SEGMENTS_FILES_LIST):
+        os.remove(SEGMENTS_FILES_LIST)
 
-def main(argv):
+def main(filepath,segments_times):
 
-    audio_filepath  = argv[0]                               # fichier audio d'origine
-    audio_file_dir  = os.path.dirname(audio_filepath)       # nom du dossier
-    ext  = os.path.splitext(audio_filepath)[1][1:]          # extension du fichier
-    if not ext:
-        ext = DEFAULT_EXTENSION
-        audio_file_name = os.path.basename(audio_filepath)
+    # Définition des variables
+    filedir    = os.path.dirname(filepath)         # nom de son répertoire
+    fileformat = os.path.splitext(filepath)[1][1:] # extension du fichier
+    if fileformat:
+        filename = os.path.basename(filepath).split("." + fileformat)[0]
     else:
-        audio_file_name = os.path.basename(audio_filepath).split("."+ext)[0] # nom du fichier sans l'extension
-    summary_file    = audio_file_name + "-summary." + ext
-    segments_filepath = argv[1]
-    tmp_dir = "._tmp_segmentation_files_" + audio_file_name
+        fileformat = DEFAULT_FORMAT
+        filename   = os.path.basename(filepath)
 
-    print "file path: " + audio_filepath
-    print "file name: " + audio_file_name
-    print "file dir:  "  + audio_file_dir
-    print "file ext:  " + ext
-    print tmp_dir
-    # print "summary file: " + summary_file
+    if not FINAL_FILE_DEST:
+        FINAL_FILE_DESTINATION = filename + FINAL_FILE_SUFFIX + "." + fileformat
+    tmp_dir = TEMP_DIR_PREFIX + filename
 
-    # Verifie que les fichiers existent
-    os.path.isfile(audio_filepath)
-    os.path.isfile(segments_filepath)
+    # print "file path:    " + filepath
+    # print "file name:    " + filename
+    # print "file dir:     " + filedir
+    # print "file format:  " + fileformat
+    # print "tmp dir:      " + tmp_dir
+    # print "summary file: " + FINAL_FILE_DESTINATION
+    # print "segments:\n" + str(segments_times)
+    # print "---------------------------------"
 
-    # Lit les temps des segments à recuperer
-    segments_times = get_segments_times(segments_filepath)
+    # Verifie que le fichier existe
+    check_file_exists(filepath)
+
+    # Lit les temps des segments à extraire
+    # segments_times = get_segments_times(segments_filepath)
     # print segments_times
 
     # Quitte si aucun segment n'est sélectionné
     if(len(segments_times) == 0):
-        print "No segment selected!"
+        printerror("No segment selected for {}!".format(filepath))
         sys.exit(1)
 
-    # Supprime le dossier temporaire s'il était resté
-    if os.path.exists(tmp_dir):
-        shutil.rmtree(tmp_dir)
+    # Extrait et sauvegarde les segments
+    number = extract_segments(audio_filepath,tmp_dir,fileformat,segments_times)
 
-    # Crée un nouveau dossier temporaire
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
-
-
-    # Récupère le contenu audio
-    # audio = get_audio_from_file(audio_filepath)
-
-    # Récupère et assemble les segments audio selectionnés
-    # Récupère les segments
-    get_segments(audio_filepath,tmp_dir,ext,segments_times)
-    concat_segments(tmp_dir,summary_file)
-
-    # audio_summary = get_segments_from_audio(segments,audio)
+    # Assemble les segments
+    concat_segments(tmp_dir,number,fileformat,FINAL_FILE_DESTINATION)
 
     # Affiche la durée totale du nouveau contenu audio
     # print "new file duration: " + str(audio_summary.duration_seconds)
-
-    # # Enregistre le nouveau fichier audio
-    # print "Saving audio summary into " + summary_file + "..."
-    # audio_summary.export(summary_file,format=EXTENSION)
-    # print "Saved!"
 
     # Supprime le dossier temporaire
     if os.path.exists(tmp_dir):
         shutil.rmtree(tmp_dir)
 
-
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    """ Traite les arguments & options """
+    parser = argparse.ArgumentParser(description=DESCRIPTION,formatter_class=RawTextHelpFormatter)
+    parser.add_argument('audiofile', nargs=1,
+                        help='Specify the audio file')
+    parser.add_argument('-f','--force', action="store_true",
+                        help='Force overwriting existing file')
+    parser.add_argument('--file', type=argparse.FileType('r'),
+                        help='Specify the file containing segments times')
+    parser.add_argument('--list', nargs=1,
+                        help='Specify the list containing segments times !! NOT FUNCTIONNAL YET !!')
+    parser.add_argument('-v', '--verbose', action="store_true", default=False,
+                        help='Display all informations, steps, and commands')
+    parser.add_argument('-q', '--quiet', action="store_true", default=False,
+                        help='Program will display only errors')
+    parser.add_argument('-d', '--destination', nargs=1,
+                        help='Define result file destination')
+    parser.add_argument('-s', '--suffix', nargs=1,
+                        help='Define a suffix for final file name')
+    args = vars(parser.parse_args())
+
+    if not args['file'] and not args['list']:
+        printerror("Must specify a file or a list argument containing times segments")
+        sys.exit(1)
+    elif args['file']:
+        segments_times = get_segments_times(args['file'])
+    else:
+        printerror("Segments times sent as list argument isn't currently functionnal. Exit.")
+        sys.exit(1)
+        # segments_times = ast.literal_eval(args['list'])
+
+    if args['force']:
+        FORCE_OVERWRITING="-y"
+
+    if args['quiet'] and args['verbose']:
+        printwarning("can't define both quiet and verbose flags as True."
+                     " Will use default flags.")
+    elif args['quiet']:
+        PRINT_STEPS = PRINT_COMMANDS = PRINT_COMMANDS_OUTPUT = False
+    elif args['verbose']:
+        PRINT_STEPS = PRINT_COMMANDS = PRINT_COMMANDS_OUTPUT = True
+
+    if args['destination']:
+        FINAL_FILE_DESTINATION = args['destination']
+    if args['suffix']:
+        FINAL_FILE_SUFFIX = args['suffix']
+
+    audio_filepath = ''.join(args['audiofile'])  # nom du fichier audio d'origine
+
+    main(audio_filepath,segments_times)
