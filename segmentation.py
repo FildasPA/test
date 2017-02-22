c890f8eaafe0753159fe06eae8e50dcc2964ad60
FildasPA <fildas.pa3@gmail.com>
2017-02-22 14:42:17 +0100

    lel

--- segmentation.py
+++ segmentation.py
@@ -18,6 +18,24 @@ from argparse import RawTextHelpFormatter
 import subprocess
 
 sys.tracebacklimit    = 1
+# Flags
+PRINT_STEPS            = True
+PRINT_COMMANDS         = False
+PRINT_COMMANDS_OUTPUT  = False
+
+DEFAULT_FORMAT         = "mp4"
+OUTPUT_FILE_NAME       = ""
+OUTPUT_FILE_SUFFIX     = "-summary"
+TEMP_DIR_PREFIX        = "._tmp_segmentation_files_"
+FORCE_OVERWRITING      = ""
+
+SEGMENTS_FILES_LIST    = "segment_files_list.txt"
+EXTRACTS_NAME          = "cut"
+FFMPEG_SILENT          = "-loglevel panic"
+
+ORIGIN_AUDIO_FILE      = ""
+TEMP_DIR               = ""
+
 
 DESCRIPTION = """
 Extract and join segments of audio and/or video file into one single file.
@@ -29,38 +47,17 @@ into a list or into a file looking as follow:
 ...
 """
 
-# Flags
-PRINT_STEPS            = True
-PRINT_COMMANDS         = False
-PRINT_COMMANDS_OUTPUT  = False
-
-DEFAULT_FORMAT         = "mp4"
-FINAL_FILE_SUFFIX      = "-summary"
-FINAL_FILE_DESTINATION = ""
-TEMP_DIR_PREFIX        = "._tmp_segmentation_files_"
-SEGMENTS_FILES_LIST    = "segment_files_list.txt"
-FFMPEG_SILENT          = "-loglevel panic"
-FORCE_OVERWRITING      = ""
-
-
 def printerror(message):
     print "Error: {}".format(message)
 
 def printwarning(message):
     print "Warning: {}".format(message)
 
-def check_file_exists(filepath):
-    """ Si le fichier n'existe pas, affiche un message d'erreur
-    et quitte le programme """
-    if not os.path.isfile(filepath):
-        printerror("file '{}' doesn't exist".format(filepath))
-        sys.exit(1)
-
-def get_segments_times(file):
+def read_segments_times(file):
     """ Retourne une liste à partir du fichier contenant les
     intervalles de temps des segments à extraire. """
     if PRINT_STEPS:
-        print "Get segments from '{}'...".format(file.name)
+        print "Read segments times from '{}'...".format(file.name)
 
     times = []
     for line in file:
@@ -143,9 +140,10 @@ def concat_segments(folder,number,ext,output_file):
     if PRINT_STEPS:
         print "Saved!"
 
+def terminate(folder):
     # Supprime le fichier contenant la liste des chemins vers les segments
-    if os.path.isfile(segments_list):
-        os.remove(segments_list)
+    if os.path.isfile(SEGMENTS_FILES_LIST):
+        os.remove(SEGMENTS_FILES_LIST)
 
     # # Supprime le dossier temporaire
     if os.path.exists(folder):
@@ -153,59 +151,64 @@ def concat_segments(folder,number,ext,output_file):
 
 def main(filepath,segments_times):
     # Définition des variables
-    fileformat = os.path.splitext(filepath)[1][1:] # extension du fichier (sans le '.')
+    fileformat = os.path.splitext(filepath)[1][1:] # extension du fichier, sans le '.'
     if fileformat:
-        filename = os.path.basename(filepath).split("." + fileformat)[0]
+        filename   = os.path.basename(filepath).split("." + fileformat)[0]
     else:
-        fileformat = DEFAULT_FORMAT
         filename   = os.path.basename(filepath)
+        fileformat = DEFAULT_FORMAT
 
-    global FINAL_FILE_DESTINATION
+    global OUTPUT_FILE_NAME
 
-    if not FINAL_FILE_DESTINATION:
-        FINAL_FILE_DESTINATION = filename + FINAL_FILE_SUFFIX + "." + fileformat
+    if OUTPUT_FILE_NAME:
+        OUTPUT_FILE_NAME = "{filename}.{format}".\
+            format(filename=OUTPUT_FILE_NAME, format=DEFAULT_FORMAT)
+    else:
+        OUTPUT_FILE_NAME = "{filename}{suffix}.{format}".\
+            format(filename=filename, suffix=FINAL_FILE_SUFFIX, format=fileformat)
+
     tmp_dir = TEMP_DIR_PREFIX + filename
 
-    # Verifie que le fichier existe
-    check_file_exists(filepath)
+    segments_times = read_segments_times(segments_times)
 
     # Quitte si aucun segment n'est sélectionné
     if(len(segments_times) == 0):
         printerror("No segment selected for {}!".format(filepath))
         sys.exit(1)
 
-    if(len(segments_times) == 1):
-        extract_segment(audio_filepath, FINAL_FILE_DESTINATION, fileformat,
-                        segments_times[0][0], segments_times[0][1])
-        return
-
     # Extrait les segments
     number = extract_segments(audio_filepath,tmp_dir,fileformat,segments_times)
 
-    # Assemble les extraits
-    concat_segments(tmp_dir,number,fileformat,FINAL_FILE_DESTINATION)
+    if number == 1:
+        shutil.move("{folder}/{filename}0.{format}".\
+                        format(tmp_dir,EXTRACTS_NAME,fileformat),
+                    OUTPUT_FILE_NAME)
+    else:
+        concat_segments(tmp_dir,number,fileformat,OUTPUT_FILE_NAME)
+
+    terminate(tmp_dir)
 
 
 if __name__ == "__main__":
     """ Traite les arguments & options """
     parser = argparse.ArgumentParser(description=DESCRIPTION,
                                      formatter_class=RawTextHelpFormatter)
-    parser.add_argument('audiofile', nargs=1,
-                        help='Specify the audio file')
+    parser.add_argument('audio_file', type=file,
+                        help="Specify the audio file to segment")
+    parser.add_argument('segments_times', type=argparse.FileType('r'),
+                        help="Specify the file containing segments times")
     parser.add_argument('-f','--force', action="store_true",
-                        help='Force overwriting existing file')
-    parser.add_argument('--file', type=argparse.FileType('r'),
-                        help='Specify the file containing segments times')
-    parser.add_argument('--list', nargs=1,
-                        help='Specify the list containing segments times !! NOT FUNCTIONNAL YET !!')
+                        help="Force overwriting existing file")
     parser.add_argument('-v', '--verbose', action="store_true", default=False,
-                        help='Display all informations, steps, and commands')
+                        help="Display all informations, steps, and commands")
     parser.add_argument('-q', '--quiet', action="store_true", default=False,
-                        help='Program will display only errors')
-    parser.add_argument('-d', '--destination', nargs=1,
-                        help='Define result file destination')
-    parser.add_argument('-s', '--suffix', nargs=1,
-                        help='Define a suffix for final file name')
+                        help="Program will display only errors")
+    parser.add_argument('-n', '--name',
+                        help="Define output file name. Erase suffix name")
+    parser.add_argument('-s', '--suffix',
+                        help="Define the ouput file name's suffix")
+    parser.add_argument('--format',
+                        help="Define the ouput file format")
     args = vars(parser.parse_args())
 
     if args['quiet'] and args['verbose']:
@@ -220,21 +223,18 @@ if __name__ == "__main__":
     if args['force']:
         FORCE_OVERWRITING="-y"
 
-    if not args['file'] and not args['list']:
-        printerror("Must specify a file or a list argument containing times segments")
-        sys.exit(1)
-    elif args['file']:
-        segments_times = get_segments_times(args['file'])
-    else:
-        printerror("Segments times sent as list argument isn't currently functionnal. Exit.")
-        sys.exit(1)
-        # segments_times = ast.literal_eval(args['list'])
+    if args['name']:
+        OUTPUT_FILE_NAME   = args['name']
+        OUTPUT_FILE_SUFFIX = ""
 
-    if args['destination']:
-        FINAL_FILE_DESTINATION = args['destination']
-    if args['suffix']:
-        FINAL_FILE_SUFFIX = args['suffix']
+    if args['suffix'] and args['name']:
+        printwarning("can't define both name and suffix, suffix will be ignored")
+    elif args['suffix']:
+        OUTPUT_FILE_SUFFIX = args['suffix']
 
-    audio_filepath = ''.join(args['audiofile'])  # nom du fichier audio d'origine
+    if args['format']:
+        DEFAULT_FORMAT = args['format']
 
-    main(audio_filepath,segments_times)
+    ORIGIN_AUDIO_FILE = args['audio_file'].name
+
+    main(args['segments_times'])
